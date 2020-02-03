29247d23ab498d0f0866ba81b2463b79c13972b9
xavki <titi>
2020-02-03 13:16:15 +0100

    add 23

---
+++ 23-gui/cli-gui.sh
@@ -0,0 +1,18 @@
+
+# xeyes
+docker run --rm -ti --net=host -e DISPLAY=:0 fr3nd/xeyes
+
+#xmind
+docker run --rm -ti --net=host -e DISPLAY=:0 mdwheele/xmind
+
+# firefox
+docker run --rm -ti --net=host -e DISPLAY=:0 jess/firefox
+
+# libreoffice
+docker run --rm -ti --net=host -e DISPLAY=:0 woahbase/alpine-libreoffice:x86_64
+
+# eclipse
+docker run --rm -ti --net=host -e DISPLAY=:0 psharkey/eclipse
+
+# gimp
+docker run --rm -ti --net=host -e DISPLAY=:0 jamesnetherton/gimp
